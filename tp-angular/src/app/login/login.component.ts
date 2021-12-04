949ac324e3b273499985fd6da28e09ff08685131
Demian <fioranidemian@gmail.com>
2021-12-04 09:35:49 -0300

    se hace la validacion de los formularios en login y register

--- tp-angular/src/app/login/login.component.ts
+++ tp-angular/src/app/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder,
     protected httpClient: HttpClient,
     private toast: HotToastService,
-    protected router:Router
+    protected router: Router
   ) {}
 
   ngOnInit(): void {
@@ -36,38 +36,53 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  goToRegistration = () =>{
+  goToRegistration = () => {
     this.router.navigate(['/registration']);
-  } 
+  };
 
   login() {
-    console.log('ejecutando signup');
-    let res: Observable<any> = this.httpClient
-      .post('http://localhost:4300/api/login', {
-        password: this.formLogin?.get('password')?.value,
-        email: this.formLogin?.get('email')?.value,
-      })
+    if (this.formLogin.valid) {
+      console.log('ejecutando signup');
+      let res: Observable<any> = this.httpClient.post(
+        'http://localhost:4300/api/login',
+        {
+          password: this.formLogin?.get('password')?.value,
+          email: this.formLogin?.get('email')?.value,
+        }
+      );
       // .pipe(share())
-      ;
-    res.subscribe(
-      (tokens) => {
-        localStorage.setItem("idToken", tokens.idToken);
-        localStorage.setItem("accesToken", tokens.accesToken);
-        this.toast.success( "Inicio de sesión exitoso", {
-          duration: 1300,
-          position: 'top-center'
-        });
-        setTimeout(() => {
-          this.router.navigate(['/']);
-        }, 1000);
-      },
-      (error) => {
-        console.log(error.error.error);
-        this.toast.error( error.error.error, {
-          duration: 3000,
-          position: 'top-center'
-        });
-      }
-    );
+      res.subscribe(
+        (tokens) => {
+          localStorage.setItem('idToken', tokens.idToken);
+          localStorage.setItem('accesToken', tokens.accesToken);
+          this.toast.success('Inicio de sesión exitoso', {
+            duration: 1300,
+            position: 'top-center',
+          });
+          setTimeout(() => {
+            this.router.navigate(['/']);
+          }, 1000);
+        },
+        (error) => {
+          console.log(error.error.error);
+          this.toast.error(error.error.error, {
+            duration: 3000,
+            position: 'top-center',
+          });
+        }
+      );
+    } else {
+      this.toast.error('Complete todos los campos antes de enviar', {
+        duration: 2000,
+        position: 'top-center',
+      });
+    }
+  }
+
+  get email() {
+    return this.formLogin?.get('email');
+  }
+  get password() {
+    return this.formLogin?.get('password');
   }
 }
--- tp-angular/src/app/registration/registration.component.ts
+++ tp-angular/src/app/registration/registration.component.ts
@@ -13,7 +13,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
   formRegistration = new FormGroup({});
@@ -22,65 +22,105 @@ export class RegistrationComponent implements OnInit {
     private formBuilder: FormBuilder,
     protected httpClient: HttpClient,
     private toast: HotToastService,
-    protected router:Router) { }
+    protected router: Router
+  ) {}
+
+  emailPattern: any = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  passwordPattern: any = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
   ngOnInit(): void {
     this.formRegistration = this.formBuilder.group({
-      name: new FormControl('', Validators.required),
-      userName: new FormControl('', Validators.required),
-      gender: new FormControl('', Validators.required),
-      birthdate: new FormControl('', Validators.required),
-      address: new FormControl('', Validators.required),
-      phone_number: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      userName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      // gender: new FormControl('', Validators.required),
+      // birthdate: new FormControl('', Validators.required),
+      address: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      // phone_number: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.emailPattern),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.passwordPattern),
+      ]),
     });
   }
 
+  register = () => {
+    if (this.formRegistration.valid) {
+      var userData = {
+        name: this.formRegistration?.get('name')?.value,
+        userName: this.formRegistration?.get('userName')?.value,
+        // "gender": this.formRegistration?.get('gender')?.value,
+        // "birthdate": this.formRegistration?.get('birthdate')?.value,
+        address: this.formRegistration?.get('address')?.value,
+        // "phone_number": this.formRegistration?.get('phone_number')?.value,
+        email: this.formRegistration?.get('email')?.value,
+        password: this.formRegistration?.get('password')?.value,
+      };
 
-  register = () =>{
-
-    var userData = {
-      "name": this.formRegistration?.get('name')?.value,
-      "userName": this.formRegistration?.get('userName')?.value,
-      "gender": this.formRegistration?.get('gender')?.value,
-      "birthdate": this.formRegistration?.get('birthdate')?.value,
-      "address": this.formRegistration?.get('address')?.value,
-      "phone_number": this.formRegistration?.get('phone_number')?.value,
-      "email": this.formRegistration?.get('email')?.value,
-      "password": this.formRegistration?.get('password')?.value,
-    }
-
-    let res: Observable<any> = this.httpClient
-      .post('http://localhost:4300/api/signup', {
-        userData,
-      })
+      let res: Observable<any> = this.httpClient.post(
+        'http://localhost:4300/api/signup',
+        {
+          userData,
+        }
+      );
       // .pipe(share())
-      ;
-    res.subscribe(
-      (res) => {
-        this.toast.success( "Registro exitoso, por favor confirme su email", {
-          duration: 5000,
-          position: 'top-center'
-        });
-        setTimeout(() => {
-          this.router.navigate(['/']);
-        }, 1000);
-      },
-      (error) => {
-        console.log('error res',error);
-        this.toast.error( error.error.error, {
-          duration: 2000,
-          position: 'top-center'
-        });
-      }
-    );
+      res.subscribe(
+        (res) => {
+          this.toast.success('Registro exitoso, por favor confirme su email', {
+            duration: 5000,
+            position: 'top-center',
+          });
+          setTimeout(() => {
+            this.router.navigate(['/']);
+          }, 1000);
+        },
+        (error) => {
+          console.log('error res', error);
+          this.toast.error(error.error.error, {
+            duration: 2000,
+            position: 'top-center',
+          });
+        }
+      );
 
-    console.log(userData);
-    return userData;
-  }
+      console.log(userData);
+      return userData;
+    } else {
+      this.toast.error('Complete todos los campos correctamente antes de enviar', {
+        duration: 2000,
+        position: 'top-center',
+      });
+      return false;
+    }
+  };
 
   goToLogin = () => {
     this.router.navigate(['/login']);
+  };
+
+  get name() {
+    console.log(this.formRegistration?.get('name')?.value);
+    return this.formRegistration?.get('name');
+  }
+  get userName() {
+    return this.formRegistration?.get('userName');
+  }
+  get address() {
+    return this.formRegistration?.get('address');
+  }
+  get email() {
+    return this.formRegistration?.get('email');
+  }
+  get password() {
+    return this.formRegistration?.get('password');
   }
 }
